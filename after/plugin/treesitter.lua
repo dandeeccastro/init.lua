6a8975784d73fa621855809f23b02e386dc118df
Danilo <dundeepessoal@gmail.com>
2023-07-17 22:31:23 -0300

    Kickstart is awesome!

--- after/plugin/treesitter.lua
+++
@@ -1,7 +0,0 @@
-require('nvim-treesitter.configs').setup({
-  ensure_installed = { "javascript", "ruby", "c", "lua", "rust" },
-  highlight = {
-    enable = true,
-    additional_vim_regex_highlighting = false,
-  },
-})
