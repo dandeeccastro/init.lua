019166316a856b0a8f933861a39f5baabb4a1c5f
dundee <dundeepessoal@gmail.com>
2024-02-23 09:15:47 -0300

    Linking in Obsidian

--- after/plugin/obsidian.lua
+++ after/plugin/obsidian.lua
@@ -11,3 +11,4 @@ vim.keymap.set('n', '<leader>osf', vim.cmd.ObsidianSearch, { desc = '[Obsidian] 
 vim.keymap.set('n', '<leader>onf', vim.cmd.ObsidianNew, { desc = '[Obsidian] [N]ew [F]ile'})
 vim.keymap.set('n', '<leader>orn', vim.cmd.ObsidianRename, { desc = '[Obsidian] [R]e[N]ame'})
 vim.keymap.set('n', '<leader>osw', vim.cmd.ObsidianWorkspace, { desc = '[Obsidian] [S]witch [W]orkspace'})
+vim.keymap.set('v', '<leader>ol', vim.cmd.ObsidianLink, { desc = '[Obsidian] [L]ink' })
