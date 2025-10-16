3c4082b61d4b804bd004d274b503300a75b265dd
dandeeccastro <dundeepessoal@gmail.com>
2025-10-16 07:15:45 -0300

    devdocs

--- init.lua
+++ init.lua
@@ -1067,6 +1067,67 @@ require("lazy").setup({
       end, { desc = "[Harpoon] Fourth file" })
     end,
   },
+  {
+    "maskudo/devdocs.nvim",
+    lazy = false,
+    dependencies = {
+      "folke/snacks.nvim",
+    },
+    cmd = { "DevDocs" },
+    keys = {
+      {
+        "<leader>ho",
+        mode = "n",
+        "<cmd>DevDocs get<cr>",
+        desc = "Get Devdocs",
+      },
+      {
+        "<leader>hi",
+        mode = "n",
+        "<cmd>DevDocs install<cr>",
+        desc = "Install Devdocs",
+      },
+      {
+        "<leader>hv",
+        mode = "n",
+        function()
+          local devdocs = require("devdocs")
+          local installedDocs = devdocs.GetInstalledDocs()
+          vim.ui.select(installedDocs, {}, function(selected)
+            if not selected then
+              return
+            end
+            local docDir = devdocs.GetDocDir(selected)
+            -- prettify the filename as you wish
+            Snacks.picker.files({ cwd = docDir })
+          end)
+        end,
+        desc = "Get Devdocs",
+      },
+      {
+        "<leader>hd",
+        mode = "n",
+        "<cmd>DevDocs delete<cr>",
+        desc = "Delete Devdoc",
+      }
+    },
+    opts = {
+      ensure_installed = {
+        -- "go",
+        "html",
+        -- "dom",
+        "http",
+        -- "css",
+        "javascript",
+        "typescript",
+        -- "rust",
+        -- some docs such as lua require version number along with the language name
+        -- check `DevDocs install` to view the actual names of the docs
+        "lua~5.1",
+        -- "openjdk~21"
+      },
+    },
+  },
   -- NOTE: The import below can automatically add your own plugins, configuration, etc from `lua/custom/plugins/*.lua`
   --    This is the easiest way to modularize your config.
   --
