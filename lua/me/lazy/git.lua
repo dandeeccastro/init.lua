1296892c92fe74e3f9ba8cc1948eebd2269cde7b
Danilo Collares <danilo.collares@v360.io>
2026-03-19 20:18:22 -0300

    removing unused files and adding experimental

--- lua/me/lazy/git.lua
+++ lua/me/lazy/git.lua
@@ -11,11 +11,4 @@ return {
                         },
                 },
         },
-        {
-                "FabijanZulj/blame.nvim",
-                lazy = false,
-                config = function()
-                        require('blame').setup {}
-                end,
-        },
 }
